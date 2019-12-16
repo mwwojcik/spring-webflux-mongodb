c435a2d4396cc5a2a884dafd0f60cc7064fe6faf
mwwojcik <mw.wojcik@gmail.com>
2019-12-17 00:29:55 +0100

    set remote profile

--- webflux/src/main/java/webflux/book/BookMongoDatabaseConfiguration.java
+++ webflux/src/main/java/webflux/book/BookMongoDatabaseConfiguration.java
@@ -4,6 +4,7 @@ import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.core.ReactiveMongoClientFactoryBean;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
@@ -13,12 +14,8 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @Configuration
 public class BookMongoDatabaseConfiguration {
 
-
+    @Profile("remote")
     public @Bean MongoClient reactiveMongoClient()  {
         return MongoClients.create("mongodb://10.22.33.78:27017/books");
     }
-/*
-    public @Bean ReactiveMongoDatabaseFactory reactiveMongoDatabaseFactory() {
-        return new SimpleReactiveMongoDatabaseFactory(MongoClients.create("mongodb://10.22.33.78:27017/books"), "books");
-    }*/
 }
